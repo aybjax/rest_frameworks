b83803c51bf73dbc7c02140c7116d285ab10c5dd
aybjax <aybat93@gmail.com>
2021-07-17 06:43:14 +0600

    sdfdsdsdfsdf

--- revel_go/app/controllers/app.go
+++ revel_go/app/controllers/app.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/revel/revel"
 )
 
@@ -8,6 +11,52 @@ type App struct {
 	*revel.Controller
 }
 
-func (c App) Index() revel.Result {
-	return c.Render()
+type Train struct {
+	ID int `json:id`
+	DriverName string `json:driver_name`
+	OperatingStatus bool `json:operating_status`
 }
+
+func (c *App) GetTrain() revel.Result {
+	var train Train
+
+	// id := c.Params.Route.Get("train-id")
+
+	train.ID, _ = strconv.Atoi("1")
+	train.DriverName = "Logan" // typa dbdan keled
+	train.OperatingStatus = true // typa dbdan keled
+
+	c.Response.Status = http.StatusOK
+
+	return c.RenderJSON(train)
+}
+
+func (c *App) CreateTrain() revel.Result {
+	var train Train
+
+	c.Params.BindJSON(&train)
+
+	// DB stuff
+
+	train.ID = 2
+
+	c.Response.Status = http.StatusCreated
+
+	return c.RenderJSON(train)
+}
+
+
+func (c *App) RemoveTrain() revel.Result {
+	var train Train
+
+	id := c.Params.Route.Get("train-id")
+
+	train.ID, _ = strconv.Atoi(id)
+	train.DriverName = "Logan" // typa dbdan keled
+	train.OperatingStatus = true // typa dbdan keled
+
+	c.Response.Status = http.StatusOK
+
+	return c.RenderText("")
+}
+
